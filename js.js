08f85f3d839cfd7d2a7d75e7d596e473915b95c2
SnGermen <144361057+SnGermen@users.noreply.github.com>
2024-01-16 15:45:30 +0100

    some changes

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
 let habbits = []
 const HabitKey = 'HabitKey'
+let globalActiveHabitId 
+
 
 const page = {
     menu: document.querySelector('.menuList'),
@@ -25,17 +27,21 @@ function saveData() {
     localStorage.setItem(HabitKey, JSON.stringify(habbits))
 }
 
-function rerendMenu(activeHabitId) {
+function rerendMenu(activeHabit) {
     document.querySelector('.menuList').innerHTML = ''
+    page.menu.innerHTML = ''
+    
     for (const habbit of habbits) {
-        const existed = document.querySelector(`[menuHabitId = '${habbit.id}']`)
+ 
+    console.log(habbit) 
+        const existed = document.querySelector(`[menuHabitId="${habbit.id}"]`)
         if (!existed) {
             const element = document.createElement('button')
             element.setAttribute('menuHabitId', habbit.id)
             element.classList.add('ielem')
             element.addEventListener('click', () => rerender(habbit.id))
             element.innerHTML = `<img src='img/${habbit.icon}.svg' alt='${habbit.name}' />`
-            if (activeHabitId === habbit.id) {
+            if (activeHabit === habbit.id) {
                 element.classList.add('active')
             }
 
@@ -43,7 +49,7 @@ function rerendMenu(activeHabitId) {
             continue
         }
 
-        if (activeHabitId === habbit.id) {
+        if (activeHabit === habbit.id) {
             existed.classList.add('active')
         } else {
             existed.classList.remove('active')
@@ -56,7 +62,7 @@ function rerendMenu(activeHabitId) {
 function rerenderHead(activeHabit){
 
     page.header.h1.innerText = activeHabit.name
-    const progress = activeHabit.days.lenght / activeHabit.target > 1
+    const progress = activeHabit.days.length / activeHabit.target > 1
         ? 100
         : activeHabit.days.length / activeHabit.target * 100
     page.header.progressName.innerText = progress.toFixed(0) + '%'
@@ -73,18 +79,21 @@ function rerenderContent(activeHabit) {
         element.innerHTML = `
             <div class="habDay">Day ${index + 1}</div>
             <div class="habbit_comment">${day.comment}</div>
-            <button class="habdelete">
+            <button class="habdelete" onclick = "Deleted(${index})"'>
                 <img src="img/delete.svg" alt="Delete day ${index + 1}">
             </button>`;
         page.context.daysContainer.appendChild(element);
     });
 
-    const toHtml = page.context.nextDay = `Day ${activeHabit.days.length + 1}`;
-    toHtml.innerText
+  page.context.nextDay.innerHTML = `Day ${activeHabit.days.length + 1}`;
+
 }
 
+
+
 function rerender(activeHabitId) {
-       const activeHabit = habbits.find((habit) => habit.id === activeHabitId) 
+    globalActiveHabitId = activeHabitId    
+    const activeHabit = habbits.find((habit) => habit.id === activeHabitId) 
        if (!activeHabitId) {
         return
     } 
@@ -93,21 +102,71 @@ function rerender(activeHabitId) {
     rerendMenu(activeHabit.id)
     rerenderHead(activeHabit)
     rerenderContent(activeHabit)
+
 }
 
 function adDays(event) {
-    const form = event.target
     event.preventDefault()
+    const form = event.target
     const data = new FormData(form)
     const comment = data.get('comment');
     form['comment'].classList.remove('error')
     if(!comment){
     form['comment'].classList.add('error')
     }
+    habbits = habbits.map(habbit =>{
+        if (habbit.id == globalActiveHabitId){
+            return{
+                ...habbit,
+                days: habbit.days.concat([{ comment }])
+            }
+        }
+        return habbit
+    })
+    form['comment'].value = ''
+    rerender(globalActiveHabitId)
+    saveData()
+    
 }
 
+function Deleted(index){
+    
+    habbits = habbits.map(habbit => {
+        if(habbit.id === globalActiveHabitId){
+             habbit.days.splice(index, 1)
+             return {
+                ...habbit,
+                    days: habbit.days
+            }
+        }
+        return habbit
+    }) 
+    rerender(globalActiveHabitId)
+    saveData()
+}
 
 (() => {
     loadData()
     rerender(habbits[0].id)
 })()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
